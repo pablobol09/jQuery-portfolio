3563f18a2b9a00242e17793130b02ea86ce7d88b
Pablo <pablobolbol09@gmail.com>
2016-12-06 21:46:55 -0800

    background pics with languages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
-
+  get 'pages/portfolio' => 'pages#portfolio'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
